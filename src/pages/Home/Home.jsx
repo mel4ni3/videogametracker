5f812209f7ffb758f5affc3309dc2c15da275df9
Mtjay <migueltejedamatos@gmail.com>
2023-01-28 20:00:26 -0500

    add game page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import GameFeed from '../../components/GameFeed/GameFeed'
+
+const Home = () =>{
+    return(
+        <div>
+            <GameFeed />
+        </div>
+    )
+}
+
+export default Home
+
