84801ee4ecbd102b78bd39fa922ff5a071d8f6cf
Mtjay <migueltejedamatos@gmail.com>
2023-01-29 09:00:48 -0500

    add backendand db

--- src/components/GameFeed/GameFeed.jsx
+++ src/components/GameFeed/GameFeed.jsx
@@ -1,12 +1,23 @@
 import Game from '../Game/Game'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+import './gamefeed.css'
 
 const GameFeed = () =>{
+    
+    const [newGame, setNewGame] = useState([]);
+
+    const fetchPopular = async () =>{
+        const res = await axios.get("http://localhost:5000/")
+        setNewGame(res.data)
+        //console.log(newGame)
+    }
+    //const popGames = fetchPopular()
+    useEffect(()=>{fetchPopular()}, [])
+    console.log(newGame[0])
     return(
         <div className='game_feed_container'>
-            <Game />
-            <Game />
-            <Game />
-            <Game />
+            {newGame.map(game=><div key={game.id}><Game gameData={game}/></div>)}
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import Home from './pages/Home/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ 
     <Home />
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
